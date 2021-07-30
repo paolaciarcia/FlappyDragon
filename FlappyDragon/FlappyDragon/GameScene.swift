03b321b70408b3777cc1d1c970b89c363a83cae2
Paola Ciarcia <paolaciarcia20@gmail.com>
2021-07-30 14:10:52 -0300

    add animations to player and floor

--- FlappyDragon/FlappyDragon/GameScene.swift
+++ FlappyDragon/FlappyDragon/GameScene.swift
@@ -12,12 +12,42 @@ class GameScene: SKScene {
  
     var floor: SKSpriteNode!
     var intro: SKSpriteNode!
+    var player: SKSpriteNode!
+    
     var gameArea: CGFloat = 410.0
+    var velocity: Double = 100.0
     
     override func didMove(to view: SKView) {
         addBackground()
         addFloor()
         addIntro()
+        addPlayer()
+        moveFloor()
+    }
+    
+    func addPlayer() {
+        player = SKSpriteNode(imageNamed: "player1")
+        player.zPosition = 4
+        player.position = CGPoint(x: 60, y: size.height - gameArea/2)
+        
+        var playerTexture = [SKTexture]()
+        for i in 1...4 {
+            playerTexture.append(SKTexture(imageNamed: "player\(i)"))
+        }
+        let animationAction = SKAction.animate(with: playerTexture, timePerFrame: 0.09)
+        let repeatAction = SKAction.repeatForever(animationAction)
+        player.run(repeatAction)
+        
+        addChild(player)
+    }
+    
+    func moveFloor() {
+        let duration = Double(floor.size.width/2) / velocity
+        let moveFloorAction = SKAction.moveBy(x: -floor.size.width/2, y: 0, duration: duration)
+        let resetXAction = SKAction.moveBy(x: floor.size.width/2, y: 0, duration: 0)
+        let sequenceAction = SKAction.sequence([moveFloorAction, resetXAction])
+        let repeatAction = SKAction.repeatForever(sequenceAction)
+        floor.run(repeatAction)
     }
     
     func addIntro() {
@@ -25,6 +55,13 @@ class GameScene: SKScene {
         intro.zPosition = 3
         intro.position = CGPoint(x: size.width/2, y: size.height - 210)
         addChild(intro)
+    }
+    
+    func addFloor() {
+        floor = SKSpriteNode(imageNamed: "floor")
+        floor.zPosition = 2
+        floor.position = CGPoint(x: floor.size.width/2, y: size.height - gameArea - floor.size.height/2)
+        addChild(floor)
     }
     
     func addBackground() {
@@ -35,12 +72,7 @@ class GameScene: SKScene {
         addChild(background)
     }
     
-    func addFloor() {
-        floor = SKSpriteNode(imageNamed: "floor")
-        floor.zPosition = 2
-        floor.position = CGPoint(x: floor.size.width/2, y: size.height - gameArea - floor.size.height/2)
-        addChild(floor)
-    }
+    
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
